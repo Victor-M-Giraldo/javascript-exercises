b0dd733611d8060cda4604c804a603f7df1bd18e
Victor Giraldo <vgiraldo@stevens.edu>
2024-03-17 12:51:05 -0400

    exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+  let ans = "";
+  for (let index = word.length - 1; index >= 0; index--) {
+    ans += word[index];
+  }
+  return ans;
 };
 
 // Do not edit below this line
