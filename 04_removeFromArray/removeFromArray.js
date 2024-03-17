810af7dc8cccf02fcad406c9f9aa366e5f70eb43
Victor Giraldo <vgiraldo@stevens.edu>
2024-03-17 13:07:41 -0400

    exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (let index = 0; index < array.length; index++) {
+        if (args.includes(array[index])) {
+            array.splice(index, 1);
+            index--;
+        }
+    }
+    return array;
 
 };
 
