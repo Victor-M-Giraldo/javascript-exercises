04636996593ebca9cb5a1d8699b187ef427d9010
Victor Giraldo <vgiraldo@stevens.edu>
2024-03-17 12:30:41 -0400

    finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = "";
+    if (num < 0) return "ERROR";
+    for (let index = 0; index < num; index++) {
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
