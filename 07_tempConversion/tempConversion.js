eb1fcdb891e26c1203927330ccfeac7252d14470
Victor Giraldo <117304729+Victor-M-Giraldo@users.noreply.github.com>
2024-03-17 23:30:09 -0400

    exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let ans = (temp - 32) * 5 / 9;
+  ans = ans.toFixed(1);
+  return parseFloat(ans);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let ans = temp * 9 / 5 + 32;
+  ans = ans.toFixed(1);
+  return parseFloat(ans);
 };
 
 // Do not edit below this line
